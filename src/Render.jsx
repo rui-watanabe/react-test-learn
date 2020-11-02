b0944a471f7f7c5ec9386e707528c45827065ce8
rui-watanabe <wataru610806@gmail.com>
2020-11-02 19:34:55 +0900

    create render.jsx

---
+++ src/Render.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Render = () => {
+  return <div>
+    <h1>React Tedt Learn</h1>
+    <input type="text" />
+    <button>Click1</button>
+    <button>Click2</button>
+    <p>Udemy</p>
+    <span>@React</span>
+  </div>;
+};
+
+export default Render;
