1e6f528cfb4b6639317cf24e86915699be813f5c
rui-watanabe <wataru610806@gmail.com>
2020-11-03 14:31:30 +0900

    create Render.test.js

---
+++ src/Render.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Render from "./Render";
+
+describe("Rendering", () => {
+  it("Should render all the elements correctly", () => {
+    render(<Render />);
+    // screen.debug();
+    // screen.debug(screen.getByRole("heading"))
+    expect(screen.getByRole("heading")).toBeTruthy();
+  })
+});
