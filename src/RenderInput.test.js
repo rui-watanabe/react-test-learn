3d6bbaecd528f07a44cfbbef3b4a796f0f1f258c
rui-watanabe <wataru610806@gmail.com>
2020-11-07 12:09:30 +0900

    test input value correctly

--- src/RenderInput.test.js
+++ src/RenderInput.test.js
@@ -3,11 +3,22 @@ import { render, screen, cleanup } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import RenderInput from "./RenderInput";
 
+//reset rendering
+afterEach(() => cleanup())
 
 describe("Rendering", () => {
   it("Should render all the elements correctly", () => {
     render(<RenderInput />)
     expect(screen.getByRole("button")).toBeTruthy();
-    expect(screen.getByPlaceholderText("Enter").toBeTruthy)
+    expect(screen.getByPlaceholderText("Enter").toBeTruthy);
   })
+})
+
+describe("Input form onChange event", () => {
+  it("should update input value correctly", () => {
+    render(<RenderInput />);
+    const inputValue = screen.getByPlaceholderText("Enter");
+    userEvent.type(inputValue, "test");
+    expect(inputValue.value).toBe("test");
+  });
 })
