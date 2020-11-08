8dc1a68ae29ee3afada7064549ab4336e9e51349
rui-watanabe <wataru610806@gmail.com>
2020-11-08 16:00:07 +0900

    create useEffectRender.jsx

---
+++ src/FrameworkList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FrameworkList = (props) => {
+  if (!props.frameworks || !props.frameworks.length) {
+    return <h1 > No data </h1>
+  }
+  return ( <div > {
+      props.frameworks.map(({ id, item }) => ( 
+        <li key = { id } > { item } </li>
+      ))
+    } </div>
+  )
+}
+
+export default FrameworkList
