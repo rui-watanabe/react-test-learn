a778b46625e898bc715b7398aaa8ab7cdc0e614e
rui-watanabe <wataru610806@gmail.com>
2020-11-09 22:26:07 +0900

    test fetched failure

--- src/MockServer.test.js
+++ src/MockServer.test.js
@@ -19,11 +19,23 @@ afterEach(() => {
 afterAll(() => server.close());
 
 describe("Mocking API", () => {
-  it("Should display fetched data correctly and button disabled", async () => {
+  it("Should display fetched data correctly and button disabled(fetch success)", async () => {
     render(<MockServer />);
     userEvent.click(screen.getByRole("button"))
     expect(await screen.findByRole("heading")).toHaveTextContent("Bred dummy")
     expect(screen.getByRole("button")).toHaveAttribute("disabled")
+  })
+  it("Should display error message, no render heading and button abled (fetched failure)", async () => {
+    server.use(
+      rest.get("https://jsonplaceholder.typicode.com/users/1", (req, res, ctx) => {
+      return res(ctx.status(404))
+  })
+    )
+    render(<MockServer />);
+    userEvent.click(screen.getByRole("button"))
+    expect(await screen.findByTestId("error")).toHaveTextContent("Fetching Failed")
+    expect(await screen.queryByRole("heading")).toBeNull()    
+    expect(screen.getByRole("button")).not.toHaveAttribute("disabled")
   })
 })
 
