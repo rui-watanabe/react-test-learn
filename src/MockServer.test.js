fd9cf36ce680571932f043b6a792e4db2018f28b
rui-watanabe <wataru610806@gmail.com>
2020-11-09 22:16:50 +0900

    test fetched success

---
+++ src/MockServer.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import MockServer from "./MockServer";
+
+const server = setupServer(
+  rest.get("https://jsonplaceholder.typicode.com/users/1", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({username: "Bred dummy"}))
+  })
+)
+
+beforeAll(() => server.listen())
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
+afterAll(() => server.close());
+
+describe("Mocking API", () => {
+  it("Should display fetched data correctly and button disabled", async () => {
+    render(<MockServer />);
+    userEvent.click(screen.getByRole("button"))
+    expect(await screen.findByRole("heading")).toHaveTextContent("Bred dummy")
+    expect(screen.getByRole("button")).toHaveAttribute("disabled")
+  })
+})
+
