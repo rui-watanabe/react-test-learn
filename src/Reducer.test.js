6fe54212c3e91b01f886ad7604564b1eccecf3a4
rui-watanabe <wataru610806@gmail.com>
2020-11-13 22:48:28 +0900

    test Should increment other mode

--- src/Reducer.test.js
+++ src/Reducer.test.js
@@ -2,14 +2,32 @@ import reducer, {increment, incrementByAmount} from '../src/features/customCount
 
 describe("Reducer of ReduxToolKit", () => {
   describe("increment action", () => {
-    let initialStatte = {
+    let initialState = {
       mode: 0,
       value: 1,
     }
     it("Should increment by 1 with mode 0", () => {
       const action = {type: increment.type};
-      const state = reducer(initialStatte, action);
+      const state = reducer(initialState, action);
       expect(state.value).toEqual(2);
+    })
+    it("Should increment by 100 with mode 1", () => {
+      initialState = {
+        mode: 1,
+        value: 1,
+      }
+      const action = {type: increment.type};
+      const state = reducer(initialState, action);
+      expect(state.value).toEqual(101);
+    })
+    it("Should increment by 10000 with mode 2", () => {
+      initialState = {
+        mode: 2,
+        value: 1,
+      }
+      const action = {type: increment.type};
+      const state = reducer(initialState, action);
+      expect(state.value).toEqual(10001);
     })
   })
 })
