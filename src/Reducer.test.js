a541cc5a1c0afe308d486085190db187f13f9f2b
rui-watanabe <wataru610806@gmail.com>
2020-11-14 13:47:46 +0900

    incrementByAmount action

--- src/Reducer.test.js
+++ src/Reducer.test.js
@@ -30,4 +30,33 @@ describe("Reducer of ReduxToolKit", () => {
       expect(state.value).toEqual(10001);
     })
   })
+  describe("incrementByAmount action", () => {
+    let initialState = {
+      mode: 0,
+      value: 1,
+    }
+    it("Should increment by payload value with mode 0", () => {
+        const action = {type: incrementByAmount.type, payload: 3};
+        const state = reducer(initialState, action);
+        expect(state.value).toEqual(4);
+    })
+    it("Should increment by 100 * 1", () => {
+      initialState = {
+        mode: 1,
+        value: 1,
+      }
+      const action = {type: incrementByAmount.type, payload: 3};
+      const state = reducer(initialState, action);
+      expect(state.value).toEqual(301);
+    })
+    it("Should increment by 10000 * payload value with mode 2", () => {
+      initialState = {
+        mode: 2,
+        value: 1,
+      }
+      const action = {type: incrementByAmount.type, payload: 3};
+      const state = reducer(initialState, action);
+      expect(state.value).toEqual(30001);
+    })
+  })
 })
