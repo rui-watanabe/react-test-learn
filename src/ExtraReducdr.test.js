a8d5b3799dd4c70ed8e083f5f6c47f852d024145
rui-watanabe <wataru610806@gmail.com>
2020-11-15 13:19:46 +0900

    test Should output 100 + payload when rejected

--- src/ExtraReducdr.test.js
+++ src/ExtraReducdr.test.js
@@ -10,4 +10,9 @@ describe("extraReducers", () => {
     const state = reducer(initialState, action);
     expect(state.value).toEqual(105);
   })
+  it("Should output 100 + payload when rejected", () => {
+    const action = { type: fetchDummy.rejected.type, payload: 5};
+    const state = reducer(initialState, action);
+    expect(state.value).toEqual(95);
+  })
 })
